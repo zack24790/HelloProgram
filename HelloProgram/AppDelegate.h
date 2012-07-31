d675985e7a360a45a9ce6b0f245f37f623bec98c
Zachary Hennessey <zack24790@yahoo.com>
2012-07-31 13:43:55 -0500

    Initial Commit

---
+++ HelloProgram/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloProgram
+//
+//  Created by Zachary Tyler Hennessey on 7/31/12.
+//  Copyright (c) 2012 Zachary Tyler Hennessey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloProgram/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloProgram
+//
+//  Created by Zachary Tyler Hennessey on 7/31/12.
+//  Copyright (c) 2012 Zachary Tyler Hennessey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
