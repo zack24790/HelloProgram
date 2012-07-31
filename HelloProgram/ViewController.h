37f5b2723122cad447a4782fdd730f7b9dfa3154
Zachary Hennessey <zack24790@yahoo.com>
2012-07-31 14:25:12 -0500

    Added Button Actions
    Added button functionality to copy text from the text field and paste
    to the label.

--- HelloProgram/ViewController.h
+++ HelloProgram/ViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *helloLabel;
 
+@property (weak, nonatomic) IBOutlet UITextField *helloTextField;
+- (IBAction)helloButtonPressed:(UIButton *)sender;
 @end
